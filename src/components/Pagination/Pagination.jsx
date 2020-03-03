e30e26330a8276064eb0e883794bd60077a2c1b0
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-03 12:31:13 +0300

    add prop-types

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Pagination.css';
 
 const Pagination = ({ pagination }) => (
@@ -6,5 +7,13 @@ const Pagination = ({ pagination }) => (
     <button type="button" className={styles.button}>{pagination}</button>
   </li>
 );
+
+Pagination.propTypes = {
+  pagination: PropTypes.number,
+};
+
+Pagination.defaultProps = {
+  pagination: [],
+};
 
 export default Pagination;
