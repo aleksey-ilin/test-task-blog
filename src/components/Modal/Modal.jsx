b6b702084ae1d39d20e7f7f33ea97f8f712d0ed3
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-05 21:04:52 +0300

    add useState for uploadPhotos

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
-import uploadPhoto from '../../assets/uploadPhoto.svg';
+import React, { useState } from 'react';
+import defaultImage from '../../assets/defaultImage.svg';
 import styles from './Modal.css';
 
-const Modal = () => (
-  <div className={styles.container}>
-    <div className={styles.modal}>
-      <button className={styles.close} type="button">×</button>
-      <h2 className={styles.heading}>Add new</h2>
-      <div className={styles.photo}>
-        <input className={styles.photo__input} type="file" accept="image/*" multiple="" />
-        <img src={uploadPhoto} alt="upload" />
-        <p className={styles.photo__description}>select an image file to upload or drag it here</p>
+const Modal = () => {
+  const [photos, uploadPhotos] = useState([]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <button className={styles.close} type="button">×</button>
+        <h2 className={styles.heading}>Add new</h2>
+        <div className={styles.photo}>
+          <input
+            className={styles.photo__input}
+            type="file"
+            accept="image/*"
+            multiple
+            onChange={(event) => uploadPhotos([...photos, ...event.target.files])}
+          />
+          <img src={defaultImage} alt="upload" />
+          <p className={styles.photo__description}>
+            select an image file to upload or drag it here
+          </p>
+        </div>
+        <label className={styles.title} htmlFor="title">
+          Title
+          <input className={styles.title__field} type="text" id="title" placeholder="Enter title" />
+        </label>
+        <label className={styles.description} htmlFor="description">
+          Description
+          <textarea className={styles.description__field} id="description" placeholder="Enter description" />
+        </label>
+        <button className={styles.button} type="button">Save</button>
       </div>
-      <label className={styles.title} htmlFor="title">
-        Title
-        <input className={styles.title__field} type="text" id="title" placeholder="Enter title" />
-      </label>
-      <label className={styles.description} htmlFor="description">
-        Description
-        <textarea className={styles.description__field} id="description" placeholder="Enter description" />
-      </label>
-      <button className={styles.button} type="button">Save</button>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
