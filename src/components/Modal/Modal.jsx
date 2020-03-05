6e3ff9bfeb3843534985f7f611c2b2fac3649414
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-05 12:57:55 +0300

    create layout modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import uploadPhoto from '../../assets/uploadPhoto.svg';
+import styles from './Modal.css';
+
+const Modal = () => (
+  <div className={styles.container}>
+    <div className={styles.modal}>
+      <button className={styles.close} type="button">×</button>
+      <h2 className={styles.heading}>Add new</h2>
+      <div className={styles.photo}>
+        <input className={styles.photo__input} type="file" accept="image/*" multiple="" />
+        <img src={uploadPhoto} alt="upload" />
+        <p className={styles.photo__description}>select an image file to upload or drag it here</p>
+      </div>
+      <label className={styles.title} htmlFor="title">
+        Title
+        <input className={styles.title__field} type="text" id="title" placeholder="Enter title" />
+      </label>
+      <label className={styles.description} htmlFor="description">
+        Description
+        <textarea className={styles.description__field} id="description" placeholder="Enter description" />
+      </label>
+      <button className={styles.button} type="button">Save</button>
+    </div>
+  </div>
+);
+
+export default Modal;
